b57e0c7cc850f47068e05427dce052f6c28941cf
Rónán Duddy <work+github@ronanduddy.com>
2024-12-07 17:01:01 +0000

    Subscribe to TurboFramePost changes
    Via broadcast methods

--- app/models/turbo_frame_post.rb
+++ app/models/turbo_frame_post.rb
@@ -1,2 +1,30 @@
 class TurboFramePost < Post
+  after_create_commit -> {
+    broadcast_prepend_to( # via a web socket rather than AJAX
+      "super_posts", # subscribers listen to this channel/stream
+      partial: "turbo_frame_posts/row",
+      locals: { post: self },
+      target: "turbo_posts" # id/frame
+    )
+  }
+
+  # the above could be reduced to the following if conventions were followed
+  # after_create_commit -> { broadcast_prepend_to "turbo_frame_posts" }
+
+
+  after_update_commit -> {
+    broadcast_replace_to(
+      "super_posts",
+      partial: "turbo_frame_posts/row",
+      locals: { post: self },
+      target: "turbo_posts"
+    )
+  }
+
+  after_destroy_commit -> {
+    broadcast_remove_to(
+      "super_posts",
+      target: "row_turbo_frame_post_#{id}"
+    )
+  }
 end
