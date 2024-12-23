5fea7a85eb80f40e6cad7ff2817a3cce928b19a9
Rónán Duddy <work+github@ronanduddy.com>
2024-12-23 13:30:32 +0000

    Introduce Company
    This will belong to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_06_131322) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_23_132824) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
